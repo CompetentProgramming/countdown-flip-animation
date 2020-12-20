f13427fd651b9a0a59bd88b4ceaf43a0a2242780
CompetentProgramming <competent.programming@gmail.com>
2020-12-20 22:08:01 +0200

    fix issue in safari

--- script.js
+++ script.js
@@ -1,100 +1,99 @@
-const days = document.querySelector('.days')
-const hours = document.querySelector('.hours')
-const minutes = document.querySelector('.minutes')
-const seconds = document.querySelector('.seconds')
-
-let timeLeft = {
-    d: 0,
-    h: 0,
-    m: 0,
-    s: 0,
-}
-
-let totalSeconds;
-
-function init() {
-    totalSeconds = Math.floor((new Date('01.01.2021') - new Date()) / 1000); 
-    setTimeLeft();
-
-    let interval = setInterval(() => {
-        if (totalSeconds < 0) {
-            clearInterval(interval);
-        }
-        countTime();
-        // console.log(timeLeft);
-    }, 1000);
-}
-
-function countTime() {
-    if (totalSeconds > 0) {
-        --timeLeft.s;
-        if (timeLeft.m >= 0 && timeLeft.s < 0) {
-            timeLeft.s = 59;
-            --timeLeft.m;
-            if (timeLeft.h >= 0 && timeLeft.m < 0) {
-                timeLeft.m = 59;
-                --timeLeft.h;
-                if (timeLeft.d >= 0 && timeLeft.h < 0) {
-                    timeLeft.h = 23;
-                    --timeLeft.d;
+window.addEventListener('load', () => {
+    const days = document.querySelector('.days')
+    const hours = document.querySelector('.hours')
+    const minutes = document.querySelector('.minutes')
+    const seconds = document.querySelector('.seconds')
+    
+    let timeLeft = {
+        d: 0,
+        h: 0,
+        m: 0,
+        s: 0,
+    }
+    
+    let totalSeconds;
+    
+    function init() {
+        totalSeconds = Math.floor((new Date('01/01/2021') - new Date()) / 1000); 
+        setTimeLeft();
+        console.log(333, timeLeft);
+        let interval = setInterval(() => {
+            if (totalSeconds < 0) {
+                clearInterval(interval);
+            }
+            countTime();
+            // console.log(timeLeft);
+        }, 1000);
+    }
+    
+    function countTime() {
+        if (totalSeconds > 0) {
+            --timeLeft.s;
+            if (timeLeft.m >= 0 && timeLeft.s < 0) {
+                timeLeft.s = 59;
+                --timeLeft.m;
+                if (timeLeft.h >= 0 && timeLeft.m < 0) {
+                    timeLeft.m = 59;
+                    --timeLeft.h;
+                    if (timeLeft.d >= 0 && timeLeft.h < 0) {
+                        timeLeft.h = 23;
+                        --timeLeft.d;
+                    }
                 }
             }
         }
+        --totalSeconds;
+        printTime();
     }
-    --totalSeconds;
-    printTime();
-}
-
-function printTime() {
-    // days.innerText = timeLeft.d;
-    // hours.innerText = timeLeft.h;
-    // minutes.innerText = timeLeft.m;
-    // seconds.innerText = timeLeft.s;
-
-    animateFlip(days, timeLeft.d);
-    animateFlip(hours, timeLeft.h);
-    animateFlip(minutes, timeLeft.m);
-    animateFlip(seconds, timeLeft.s);
-}
-
-function animateFlip(element, value) {
-    const valueInDom = element.querySelector('.bottom-back').innerText;
-    const currentValue = value < 10 ? '0' + value : '' + value;
-
-    if (valueInDom === currentValue) return;
-
-
-    element.querySelector('.top-back span').innerText = currentValue;
-    element.querySelector('.bottom-back span').innerText = currentValue;
-
-
-    gsap.to(element.querySelector('.top'), 0.7, {
-        rotationX: '-180deg',
-        transformPerspective: 300,
-        ease: Quart.easeOut,
-        onComplete: function() {
-            element.querySelector('.top').innerText = currentValue; 
-            element.querySelector('.bottom').innerText = currentValue; 
-            gsap.set(element.querySelector('.top'), {rotationX: 0});
-        }
-    });
-
-    gsap.to(element.querySelector('.top-back'), 0.7, {
-        rotationX: 0,
-        transformPerspective: 300,
-        ease: Quart.easeOut,
-        clearProps: 'all'
-    });
-
-}
-
-
-
-function setTimeLeft() {
-    timeLeft.d = Math.floor(totalSeconds / (60 * 60 * 24));
-    timeLeft.h = Math.floor(totalSeconds / (60 * 60) % 24);
-    timeLeft.m = Math.floor(totalSeconds / (60) % 60);
-    timeLeft.s = Math.floor(totalSeconds % 60);
-}
-
-init();
+    
+    function printTime() {
+        animateFlip(days, timeLeft.d);
+        animateFlip(hours, timeLeft.h);
+        animateFlip(minutes, timeLeft.m);
+        animateFlip(seconds, timeLeft.s);
+    }
+    
+    function animateFlip(element, value) {
+        const valueInDom = element.querySelector('.bottom-back').innerText;
+        const currentValue = value < 10 ? '0' + value : '' + value;
+    
+        if (valueInDom === currentValue) return;
+    
+        console.log(111, currentValue);
+        console.log(222, value);
+        element.querySelector('.top-back span').innerText = currentValue;
+        element.querySelector('.bottom-back span').innerText = currentValue;
+    
+    
+        gsap.to(element.querySelector('.top'), 0.7, {
+            rotationX: '-180deg',
+            transformPerspective: 300,
+            ease: Quart.easeOut,
+            onComplete: function() {
+                element.querySelector('.top').innerText = currentValue; 
+                element.querySelector('.bottom').innerText = currentValue; 
+                gsap.set(element.querySelector('.top'), {rotationX: 0});
+            }
+        });
+    
+        gsap.to(element.querySelector('.top-back'), 0.7, {
+            rotationX: 0,
+            transformPerspective: 300,
+            ease: Quart.easeOut,
+            clearProps: 'all'
+        });
+    
+    }
+    
+    
+    
+    function setTimeLeft() {
+        console.log(444, totalSeconds);
+        timeLeft.d = Math.floor(totalSeconds / (60 * 60 * 24));
+        timeLeft.h = Math.floor(totalSeconds / (60 * 60) % 24);
+        timeLeft.m = Math.floor(totalSeconds / (60) % 60);
+        timeLeft.s = Math.floor(totalSeconds % 60);
+    }
+    
+    init();
+});
