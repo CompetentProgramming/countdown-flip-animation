2c501288e17f86a629a1df3988e9479d80b19340
CompetentProgramming <competent.programming@gmail.com>
2020-12-20 22:09:40 +0200

    change the year to 2021

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener('load', () => {
     let totalSeconds;
     
     function init() {
-        totalSeconds = Math.floor((new Date('01/01/2022') - new Date()) / 1000); 
+        totalSeconds = Math.floor((new Date('01/01/2021') - new Date()) / 1000); 
         setTimeLeft();
         let interval = setInterval(() => {
             if (totalSeconds < 0) {
